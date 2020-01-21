212a254dea2db7efecfa98b54ffac361199516fd
Chris Magorian <chrismagorian@gmail.com>
2020-01-20 22:15:09 -0800

    add docker install to fetch_dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,6 +21,7 @@ JOIN_TOKEN_CERT_HASH=""
 fetch_dependencies() {
   apt-get update && apt-get install -y apt-transport-https curl \
       apt-transport-https ca-certificates curl software-properties-common gnupg2
+  curl -sSL https://get.docker.com | sh
   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
   cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
   deb https://apt.kubernetes.io/ kubernetes-xenial main
